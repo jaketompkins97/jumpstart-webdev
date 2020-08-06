115d04628b127d33c4f13b7c818c80ebb69e692b
Jake Tompkins <jaketompkins97@g.ucla.edu>
2020-08-05 18:57:03 -0700

    first commit of the webdev app

---
+++ app.py
@@ -0,0 +1,21 @@
+# app.py
+
+from flask import Flask, render_template, redirect, url_for, jsonify
+import poembot
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello world!"
+
+@app.route('/poem')
+def poem():
+    poem = poembot.generate_poem()
+    return poem
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
